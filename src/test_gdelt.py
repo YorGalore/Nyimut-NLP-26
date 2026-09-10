d3f2a6ca10183ddbbd07b0c59c8a6e740f2a0cbc
Yor <freyorla28@gmail.com>
2026-09-10 20:35:44 +0700

    menambahkan isi test_gdelt

--- src/test_gdelt.py
+++ src/test_gdelt.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import requests
+
+url = "https://api.gdeltproject.org/api/v2/doc/doc"
+p = {"query": "sanctions sourcelang:english",
+     "mode": "ArtList", "format": "json", "maxrecords": 10,
+     "startdatetime": "20211001000000", "enddatetime": "20211002000000"}
+
+r = requests.get(url, params=p, headers={"User-Agent": "riset-kampus"})
+print(r.status_code, r.text[:500])
